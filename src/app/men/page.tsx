1a750ec631d10a61019e5a96e541fecd44fc256c
ilonaK <ikosko@alfabank.ru>
2023-05-20 22:32:20 +0300

    feat: add base routing

---
+++ src/app/men/page.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Men',
+  description: 'Men clothes',
+};
+
+const Men: FC = () => {
+  return <h1>Men</h1>;
+};
+
+export default Men;
