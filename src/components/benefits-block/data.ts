4dc673676f56245debe851373f3331e75d8817e7
Ilona <79661554+psycho0sis@users.noreply.github.com>
2023-05-21 15:17:37 +0300

    Create benefits block (#10)
    * feat: create benefits block
    
    * feat: create benefits block

---
+++ src/components/benefits-block/data.ts
@@ -0,0 +1,20 @@
+export const benefits = [
+  {
+    id: 0,
+    image: './images/free-shipping.svg',
+    title: 'FREE SHIPPING',
+    description: 'On all UA order or order above $100',
+  },
+  {
+    id: 1,
+    image: './images/3-days-return.svg',
+    title: '30 DAYS RETURN',
+    description: 'Simply return it within 30 days for an exchange',
+  },
+  {
+    id: 2,
+    image: './images/support.svg',
+    title: 'SUPPORT 24/7',
+    description: 'Contact us 24 hours a day, 7 days a week',
+  },
+];
---
+++ src/components/benefits-block/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { flex, font } from 'styles/mixins';
+
+export const BenefitsBlockWrapper = styled.div`
+  width: 100%;
+  border-bottom: 1px solid var(--gr-white);
+`;
+
+export const BenefitsContentWrapper = styled.div`
+  ${flex({ gap: '36px' })};
+
+  @media screen and (width <= 768px) {
+    ${flex({ gap: '0px', direction: 'column' })};
+  }
+`;
+
+export const BenefitsItemWrapper = styled.div`
+  ${flex({ alignment: 'start', gap: '13px' })};
+
+  padding: 32px 10px;
+
+  @media screen and (width <= 768px) {
+    ${flex({ direction: 'column' })};
+  }
+`;
+
+export const BenefitsTextWrapper = styled.div`
+  ${flex({ alignment: 'start', direction: 'column' })};
+
+  @media screen and (width <= 768px) {
+    ${flex({ alignment: 'center', direction: 'column' })};
+  }
+`;
+
+export const Title = styled.div`
+  ${font({ lineHeight: 13, weight: 600 })};
+
+  margin-bottom: 4px;
+`;
+
+export const Description = styled.div`
+  ${font({ lineHeight: 22, weight: 500 })};
+`;
