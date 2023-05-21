7cf01eb8f9a6b27671fa86e7ab927d4e2b6b3006
ilonaK <ikosko@alfabank.ru>
2023-05-21 14:28:32 +0300

    refactor: rewrite function decraration to arrow function

--- src/app/women/[id]/page.tsx
+++ src/app/women/[id]/page.tsx
@@ -6,11 +6,11 @@ interface IProps {
   };
 }
 
-export function generateMetadata({ params: { id } }: IProps) {
+export const generateMetadata = ({ params: { id } }: IProps) => {
   return {
     title: `Product ${id}`,
   };
-}
+};
 
 const ProductPage: FC<IProps> = ({ params: { id } }) => {
   return <div>product {id}</div>;
