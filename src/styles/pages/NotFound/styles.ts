4e0cc09bf865fd83e0b9993f610f44b1b0f32204
JDM <shchuka_pasha@mail.ru>
2023-05-23 14:59:12 +0300

    feat: add not found page, add not found page styles

---
+++ src/styles/pages/NotFound/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  height: 100%;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const Title = styled.h1`
+  color: var(--dark);
+  font-size: 70px;
+  text-align: center;
+`;
+
+export const Subtitle = styled.p`
+  color: var(--black);
+  font-size: 30px;
+  text-align: center;
+  text-decoration: underline;
+`;
